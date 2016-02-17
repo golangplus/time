4cae28fd4eb8ca49786222b133ddfd916b7537be
David Deng <daviddengcn@gmail.com>
2016-02-17 07:27:48 -0800

    Add testcase for SleepUntil

---
+++ time_test.go
@@ -0,0 +1,20 @@
+package timep
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golangplus/testing/assert"
+)
+
+func TestSleepUntil(t *testing.T) {
+	tm := time.Now().Add(time.Second)
+	SleepUntil(tm)
+	assert.ValueShould(t, "now", time.Now(), tm.Before, "SleepUntil returns earlier than expected")
+}
+
+func TestSleepUntil_SleepBefore(t *testing.T) {
+	start := time.Now()
+	SleepUntil(start)
+	assert.ValueShould(t, "now", time.Now(), start.Add(time.Millisecond).After, "SleepUntil returns later than expected")
+}
